671bb5924d2d5f5ee7a1ea9209caa9803f1bc034
2019211001001201-JieMengyao <2190519274@qq.com>
2021-03-14 20:31:20 +0800

    HW-week 2 commit

---
+++ src/main/java/com/JieMengyao/week2/demo/assignment1.java
@@ -0,0 +1,23 @@
+package com.JieMengyao.week2.demo;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+public class assignment1 extends HttpServlet {
+    String name="JieMengyao";
+    String id="2019211001001201";
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        PrintWriter writer=response.getWriter();
+        response.setContentType("text/html;charset=utf-8");
+        Date date=new Date();
+        writer.println("name: "+name+"<br/>");
+        writer.println("id: "+id+"<br/>");
+        writer.println("Date and Time: "+date.toString()+"<br/>");
+    }
+    public void doPost(HttpServletRequest request, HttpServletResponse response) {
+
+    }
+}
